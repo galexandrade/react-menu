cffcd477aa9851b646042704ee3915aa0fc89d68
Zheng Song <41896553+szhsin@users.noreply.github.com>
2022-01-09 18:38:09 +1100

    Update docs site to React 18

--- example/src/index.js
+++ example/src/index.js
@@ -3,8 +3,13 @@ import '@szhsin/react-menu/dist/theme-dark.css';
 import '@szhsin/react-menu/dist/transitions/slide.css';
 import './styles/index.scss';
 
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
